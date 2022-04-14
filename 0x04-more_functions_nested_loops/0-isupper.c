01d8cf5b0771760be8d6b0998256e678dc8e674d
thereson <gerardokolie09@gmail.com>
2022-04-14 06:32:09 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <main.h>
 
+/**
+ * _isdigit - checks if parameter is a number between 0 to 9.
+ * @c: input number.
+ * Return: 1 if is a number (0 to 9), 0 in other case.
+ */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
