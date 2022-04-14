a2271fac34109669c34380b3358702b16ec8df70
thereson <gerardokolie09@gmail.com>
2022-04-14 08:02:47 -0700

    +

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ *print number from 0-9
+ *int declare variable
+ *Return 0 (success)
+ */
+void print_numbers(void)
+{
+	int x;
+	
+	for(x = 0; x <= 9; x++)
+	{
+		_putchar(x);
+	}
+	        _putchar(\n);
+		return (0)
+}
